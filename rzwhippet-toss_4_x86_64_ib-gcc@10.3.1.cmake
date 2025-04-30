8e66e46c24907e019b50513f54a034d4eb0b31e9
Brian Manh Hien Han <han12@llnl.gov>
2025-04-30 11:08:46 -0700

    Very basic nanobind example working

---
+++ rzwhippet-toss_4_x86_64_ib-gcc@10.3.1.cmake
@@ -0,0 +1,151 @@
+#------------------------------------------------------------------------------
+# !!!! This is a generated file, edit at own risk !!!!
+#------------------------------------------------------------------------------
+# CMake executable path: /usr/tce/bin/cmake
+#------------------------------------------------------------------------------
+
+set(CMAKE_PREFIX_PATH "/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/blt-0.7.0-cr5g5m5cxzvebo7bmxyupsw7ajhjntmm;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/c2c-1.8.0-v2ka4tmm7e3yv3ha7j3h6raznzhje5j7;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/caliper-2.12.1-cg4ruv3s55hzcae5ix6wdld42ykittj3;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/conduit-git.2eec1ad9548cf6d629e2145181ff7b8cef0a38fa_0.9.3-ans6wjuvpe2l54yuzhuwe6tlqjx2v5dp;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/mfem-4.7.0-par5ezelpje35rgyrdgvojozdpxqrv26;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/py-jsonschema-4.17.3-pjihw6k6fkyn7jg4ehmh6zap6jg66xzt;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/raja-2025.03.0-ez5qr5svwvswh772skkkkxpsxypnotiq;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/scr-3.0.1-2xjioxzwiuwwf53ec7b6onyx73tqnyyd;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/umpire-2025.03.0-ic6dwb3qlmhxin2fvvnomvyjfvn6c44r;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/adiak-0.4.0-g4mfjrtioqtxbtaztefksqyoozb6b36l;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/libunwind-1.8.1-nqszm3swgkebwsgsn6jyz4z6pyqrraeo;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/hdf5-1.8.23-gyr52wxsbtguhcsyxjntmccy3ax62i4j;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/parmetis-4.0.3-zrwzdydp426h4h4ckrku6cpwglykuj7m;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/hypre-2.24.0-h42ef52ikfazwv5gncdavi4hhnq75hip;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/axl-0.7.1-uvgagq7sumuh7ntervc7r2z4o6r7qvda;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/dtcmp-1.1.5-d7ytrnqekwi4wjrmvbpnxyohek6c4oy5;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/er-0.3.0-qc4ru5lce7ivmo6tekqkphy734ctaoud;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/libyogrt-1.35-4ub5lfpppsjojzsdu2lu56y72n4oq45s;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/spath-0.2.0-zi5hiiesayaa7dxrljjgii2f7pxgzf4t;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/camp-2025.03.0-ovatfp5yjgcfjjas6vafeguu5bdbomrq;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/fmt-11.0.2-ouhqrqn4aesd62bwjpzmptt7jg6jk5su;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/metis-5.1.0-n3c64uvtuyyhupblayy3jushn5ngstvs;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/lwgrp-1.0.6-rimqoqcymsh4hld7daona7kgfciqnhil;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/redset-0.2.0-syu545jov6u7nlxhcm5g5e6uhbkcjhv5;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/shuffile-0.2.0-5tirjcextoiivgek3fsdwnl2lws2ifcg;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/rankstr-0.2.0-cvvb46bi6ktyrh75bsa5w7mxipoy2z6d;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/kvtree-1.3.0-tcdwbbhk7kmdylkzrot3gbeterijm4up;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/gcc-runtime-10.3.1-pw776bbh2h5w6ulv4qr23jc3sxqq6dkk;/collab/usr/gapps/shroud/public/toss_4_x86_64_ib/shroud-0.13.0;/usr/workspace/han12/axom/devtools_nanobind/._view/grtaettd7am57i52r36qn2anxjpb5obt/python-3.12.5;/usr/tce;/collab/usr/gapps/axom/devtools/toss_4_x86_64_ib/latest/cppcheck-2.9;/collab/usr/gapps/axom/devtools/toss_4_x86_64_ib/latest/doxygen-1.9.6;/usr/tce/packages/clang/clang-14.0.6;/usr/tce/packages/mvapich2/mvapich2-2.3.6-gcc-10.3.1;/usr/workspace/han12/axom/devtools_nanobind/._view/grtaettd7am57i52r36qn2anxjpb5obt/python-3.12.5;/usr/tce/packages/mvapich2/mvapich2-2.3.7-gcc-10.3.1" CACHE STRING "")
+
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH "ON" CACHE STRING "")
+
+set(CMAKE_BUILD_RPATH "/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/axom-develop-jwgtfketwpbqhiibkb4jmnrdfbwwpurs/lib;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/axom-develop-jwgtfketwpbqhiibkb4jmnrdfbwwpurs/lib64;;/collab/usr/global/tools/tce4/packages/gcc/gcc-10.3.1/lib/gcc/x86_64-redhat-linux/10" CACHE STRING "")
+
+set(CMAKE_INSTALL_RPATH "/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/axom-develop-jwgtfketwpbqhiibkb4jmnrdfbwwpurs/lib;/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1/axom-develop-jwgtfketwpbqhiibkb4jmnrdfbwwpurs/lib64;;/collab/usr/global/tools/tce4/packages/gcc/gcc-10.3.1/lib/gcc/x86_64-redhat-linux/10" CACHE STRING "")
+
+set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
+
+#------------------------------------------------------------------------------
+# Compilers
+#------------------------------------------------------------------------------
+# Compiler Spec: gcc@=10.3.1
+#------------------------------------------------------------------------------
+if(DEFINED ENV{SPACK_CC})
+
+  set(CMAKE_C_COMPILER "/usr/WS1/han12/axom/uberenv_nanobind/spack/lib/spack/env/gcc/gcc" CACHE PATH "")
+
+  set(CMAKE_CXX_COMPILER "/usr/WS1/han12/axom/uberenv_nanobind/spack/lib/spack/env/gcc/g++" CACHE PATH "")
+
+  set(CMAKE_Fortran_COMPILER "/usr/WS1/han12/axom/uberenv_nanobind/spack/lib/spack/env/gcc/gfortran" CACHE PATH "")
+
+else()
+
+  set(CMAKE_C_COMPILER "/usr/tce/packages/gcc/gcc-10.3.1/bin/gcc" CACHE PATH "")
+
+  set(CMAKE_CXX_COMPILER "/usr/tce/packages/gcc/gcc-10.3.1/bin/g++" CACHE PATH "")
+
+  set(CMAKE_Fortran_COMPILER "/usr/tce/packages/gcc/gcc-10.3.1/bin/gfortran" CACHE PATH "")
+
+endif()
+
+set(ENABLE_FORTRAN ON CACHE BOOL "")
+
+#------------------------------------------------------------------------------
+# MPI
+#------------------------------------------------------------------------------
+
+set(MPI_C_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.3.6-gcc-10.3.1/bin/mpicc" CACHE PATH "")
+
+set(MPI_CXX_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.3.6-gcc-10.3.1/bin/mpicxx" CACHE PATH "")
+
+set(MPI_Fortran_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.3.6-gcc-10.3.1/bin/mpif90" CACHE PATH "")
+
+set(MPIEXEC_NUMPROC_FLAG "-n" CACHE STRING "")
+
+set(ENABLE_MPI ON CACHE BOOL "")
+
+set(MPIEXEC_EXECUTABLE "/usr/bin/srun" CACHE PATH "")
+
+#------------------------------------------------------------------------------
+# Hardware
+#------------------------------------------------------------------------------
+
+#------------------------------------------------
+# Hardware Specifics
+#------------------------------------------------
+
+set(ENABLE_OPENMP ON CACHE BOOL "")
+
+set(ENABLE_GTEST_DEATH_TESTS ON CACHE BOOL "")
+
+#------------------------------------------------------------------------------
+# TPLs
+#------------------------------------------------------------------------------
+
+set(TPL_ROOT "/usr/WS1/han12/axom/uberenv_nanobind/gcc-10.3.1" CACHE PATH "")
+
+set(CONDUIT_DIR "${TPL_ROOT}/conduit-git.2eec1ad9548cf6d629e2145181ff7b8cef0a38fa_0.9.3-ans6wjuvpe2l54yuzhuwe6tlqjx2v5dp" CACHE PATH "")
+
+set(C2C_DIR "${TPL_ROOT}/c2c-1.8.0-v2ka4tmm7e3yv3ha7j3h6raznzhje5j7" CACHE PATH "")
+
+set(MFEM_DIR "${TPL_ROOT}/mfem-4.7.0-par5ezelpje35rgyrdgvojozdpxqrv26" CACHE PATH "")
+
+set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.23-gyr52wxsbtguhcsyxjntmccy3ax62i4j" CACHE PATH "")
+
+set(LUA_DIR "/usr" CACHE PATH "")
+
+set(RAJA_DIR "${TPL_ROOT}/raja-2025.03.0-ez5qr5svwvswh772skkkkxpsxypnotiq" CACHE PATH "")
+
+set(UMPIRE_DIR "${TPL_ROOT}/umpire-2025.03.0-ic6dwb3qlmhxin2fvvnomvyjfvn6c44r" CACHE PATH "")
+
+# OPENCASCADE not built
+
+set(ADIAK_DIR "${TPL_ROOT}/adiak-0.4.0-g4mfjrtioqtxbtaztefksqyoozb6b36l" CACHE PATH "")
+
+set(CALIPER_DIR "${TPL_ROOT}/caliper-2.12.1-cg4ruv3s55hzcae5ix6wdld42ykittj3" CACHE PATH "")
+
+set(CAMP_DIR "${TPL_ROOT}/camp-2025.03.0-ovatfp5yjgcfjjas6vafeguu5bdbomrq" CACHE PATH "")
+
+set(SCR_DIR "${TPL_ROOT}/scr-3.0.1-2xjioxzwiuwwf53ec7b6onyx73tqnyyd" CACHE PATH "")
+
+set(KVTREE_DIR "${TPL_ROOT}/kvtree-1.3.0-tcdwbbhk7kmdylkzrot3gbeterijm4up" CACHE PATH "")
+
+set(DTCMP_DIR "${TPL_ROOT}/dtcmp-1.1.5-d7ytrnqekwi4wjrmvbpnxyohek6c4oy5" CACHE PATH "")
+
+set(SPATH_DIR "${TPL_ROOT}/spath-0.2.0-zi5hiiesayaa7dxrljjgii2f7pxgzf4t" CACHE PATH "")
+
+set(AXL_DIR "${TPL_ROOT}/axl-0.7.1-uvgagq7sumuh7ntervc7r2z4o6r7qvda" CACHE PATH "")
+
+set(LWGRP_DIR "${TPL_ROOT}/lwgrp-1.0.6-rimqoqcymsh4hld7daona7kgfciqnhil" CACHE PATH "")
+
+set(ER_DIR "${TPL_ROOT}/er-0.3.0-qc4ru5lce7ivmo6tekqkphy734ctaoud" CACHE PATH "")
+
+set(RANKSTR_DIR "${TPL_ROOT}/rankstr-0.2.0-cvvb46bi6ktyrh75bsa5w7mxipoy2z6d" CACHE PATH "")
+
+set(REDSET_DIR "${TPL_ROOT}/redset-0.2.0-syu545jov6u7nlxhcm5g5e6uhbkcjhv5" CACHE PATH "")
+
+set(SHUFFILE_DIR "${TPL_ROOT}/shuffile-0.2.0-5tirjcextoiivgek3fsdwnl2lws2ifcg" CACHE PATH "")
+
+set(LIBYOGRT_DIR "${TPL_ROOT}/libyogrt-1.35-4ub5lfpppsjojzsdu2lu56y72n4oq45s" CACHE PATH "")
+
+#------------------------------------------------------------------------------
+# Devtools
+#------------------------------------------------------------------------------
+
+set(DEVTOOLS_ROOT "/collab/usr/gapps/axom/devtools/toss_4_x86_64_ib/2023_10_17_16_15_32/._view/ypfidjpdm7fhkqcpekza67w5xgiaw7yg" CACHE PATH "")
+
+set(CLANGFORMAT_EXECUTABLE "/usr/tce/packages/clang/clang-14.0.6/bin/clang-format" CACHE PATH "")
+
+#set(Python_ROOT_DIR "/usr/WS1/han12/axom/devtools_nanobind/gcc-10.3.1/python-3.12.5-ifx4byqsxyec4zegcjie3xmq2ysconq2" CACHE PATH "")
+#set(PYTHON_EXECUTABLE "/usr/WS1/han12/axom/devtools_nanobind/gcc-10.3.1/python-3.12.5-ifx4byqsxyec4zegcjie3xmq2ysconq2/bin/python3.12" CACHE PATH "")
+#set(Python_EXECUTABLE "/usr/WS1/han12/axom/devtools_nanobind/gcc-10.3.1/python-3.12.5-ifx4byqsxyec4zegcjie3xmq2ysconq2/bin/python3.12" CACHE PATH "")
+
+set(Python_ROOT_DIR "/usr/workspace/han12/axom/devtools_nanobind/._view/grtaettd7am57i52r36qn2anxjpb5obt/python-3.12.5" CACHE PATH "")
+set(Python_EXECUTABLE "/usr/workspace/han12/axom/devtools_nanobind/._view/grtaettd7am57i52r36qn2anxjpb5obt/python-3.12.5/bin/python3.12" CACHE PATH "")
+set(PYTHON_EXECUTABLE "/usr/workspace/han12/axom/devtools_nanobind/._view/grtaettd7am57i52r36qn2anxjpb5obt/python-3.12.5/bin/python3.12" CACHE PATH "")
+
+#set(nanobind_DIR "/usr/workspace/han12/axom/devtools_nanobind/gcc-10.3.1/py-nanobind-2.0.0-twr5gieidykafu2jik635l37vhzcs7j4/lib/python3.12/site-packages/nanobind/cmake")
+set(nanobind_ROOT "/usr/workspace/han12/axom/devtools_nanobind/._view/grtaettd7am57i52r36qn2anxjpb5obt/python-3.12.5/lib/python3.12/site-packages/nanobind/cmake" CACHE PATH "")
+
+set(JSONSCHEMA_EXECUTABLE "${TPL_ROOT}/py-jsonschema-4.17.3-pjihw6k6fkyn7jg4ehmh6zap6jg66xzt/bin/jsonschema" CACHE PATH "")
+
+set(ENABLE_DOCS ON CACHE BOOL "")
+
+set(SPHINX_EXECUTABLE "/usr/WS1/han12/axom/devtools_nanobind/._view/grtaettd7am57i52r36qn2anxjpb5obt/python-3.12.5/bin/sphinx-build" CACHE PATH "")
+
+set(SHROUD_EXECUTABLE "/collab/usr/gapps/shroud/public/toss_4_x86_64_ib/shroud-0.13.0/bin/shroud" CACHE PATH "")
+
+set(CPPCHECK_EXECUTABLE "${DEVTOOLS_ROOT}/cppcheck-2.9/bin/cppcheck" CACHE PATH "")
+
+set(DOXYGEN_EXECUTABLE "${DEVTOOLS_ROOT}/doxygen-1.9.6/bin/doxygen" CACHE PATH "")
+
+
